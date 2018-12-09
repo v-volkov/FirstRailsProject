63ee7502b8e4872de53fe062fc086c5358b339d6
volkov-v <volkov.vladyslav.2018@gmail.com>
2018-12-09 17:40:28 +0200

    added authorization

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Successfully created user!"
+      redirect_to '/contacts'
+    else
+      flash[:warning] = "Invalid Email or password"
+      redirect_to '/signup'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'contacts' => 'contacts#index'
+  # get '/contacts/new' => 'contacts#new'
+  # post '/contacts' => 'contacts#create'
+  # get '/contacts/:id' => 'contacts#show'
+  # get '/contacts/:id/edit' => 'contacts#edit'
+  # patch '/contacts/:id' => 'contacts#update'
+  # delete '/contacts/:id' => 'contacts#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
